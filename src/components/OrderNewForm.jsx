1f2e8f3a3f2255aa0d3ab1027352e0d186f238e3
David Ferreira <mrjdavidfg@gmail.com>
2019-05-15 10:14:24 -0500

    More components and feaures...

--- src/components/OrderNewForm.jsx
+++ src/components/OrderNewForm.jsx
@@ -1,49 +1,119 @@
 import React, { useState, useEffect } from 'react'
-import { Modal, Form, Input, Row, Col, Icon } from 'antd'
+import { Modal, Form, Input, Row, Col, Icon, Step, Steps, DatePicker, Select } from 'antd'
+import moment from 'moment'
+
+
+function range(start, end) {
+  const result = [];
+  for (let i = start; i < end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function disabledDate(current) {
+  // Can not select days before today and today
+  return current && current < moment().endOf('day')
+}
+
+function disabledTime() {
+  return {
+    disabledHours: () => range(0, 8).concat(range(16, 24))
+  }
+}
+
+const modalTitle = function(props) {
+  const [currentStep, setCurrentStep] = useState(0)
+  const [products, _setProducts] = useState([])
+  const steps = [
+    {
+      title: 'Form',
+      content: 'First-content',
+    },
+    {
+      title: 'Review',
+      content: 'Second-content',
+    }
+  ]
+
+  return (
+    <Steps size="small" current={current}> 
+      {steps.map(item => (
+        <Step key={item.title} title={item.title} />
+      ))}
+    </Steps>
+  )
+}
 
 export default Form.create({ name: 'new_order_form' })(function(props) {
   const { visible, onCancel, onCreate, form } = props
 
   const { getFieldDecorator } = form
 
+  const config = {
+    rules: [{ type: 'object', required: true, message: 'Please select date!' }],
+  }
+
   return (
     <Modal
       visible={visible}
-      title="New Order"
+      title={modalTitle}
       onOk={onCreate}
       onCancel={onCancel}
     >
       <Form layout="vertical">
         <Row>
-          <Col span={12}>H</Col>
-          <Col span={12}>A</Col>
+          <Col span={6}>H</Col>
+            <Form.Item label="Due">
+              {getFieldDecorator('dueDate', config)(<DatePicker disabledDate={disabledDate} />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('dueTime', config)(<TimePicker use12Hours format="h:mm a" disabledTime={disabledTime} minuteStep={60} />)}
+            </Form.Item>
+          <Col span={12}>
+            <Form.Item label="Customer">
+              {getFieldDecorator('customer', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input the name of the customer!'
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Aditional Details">
+              {getFieldDecorator('customer', {
+                rules: [
+                  {
+                    required: false
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Row>
+              <Col span={18}>
+                <Select placeholder="Select a product">
+                 {products.map(p => <Select.Option value={p.id} key={p.id}>{p.name}</Select.Option>)}
+                </Select>
+              </Col>
+              <Col span={6}>
+                <Form.Item label="Customer">
+                  {getFieldDecorator('customer', {
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Please input the name of the customer!'
+                      }
+                    ]
+                  })(<Input />)}
+                </Form.Item>
+                <InputNumber min={1} max={10} step={1} />
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6}>A</Col>
         </Row>
-        <Form.Item label="Name">
-          {getFieldDecorator('name', {
-            rules: [
-              {
-                required: true,
-                message: 'Please input the name of the product!'
-              }
-            ]
-          })(<Input placeholder="Name" />)}
-        </Form.Item>
-        <Form.Item label="Price">
-          {getFieldDecorator('price', {
-            initialValue: product ? product.price : undefined,
-            rules: [
-              {
-                required: true,
-                message: 'Please input the price of the product!'
-              }
-            ]
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Price"
-            />
-          )}
-        </Form.Item>
+        
       </Form>
     </Modal>
   )
